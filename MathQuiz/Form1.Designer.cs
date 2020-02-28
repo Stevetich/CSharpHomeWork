5a106b2011ac7f0ca0e65a5b5269b0029f973337
嘉程 张 <694510779@qq.com>
2020-02-28 09:11:15 +0800

    add MathQuiz

---
+++ MathQuiz/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace MathQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.Equal1 = new System.Windows.Forms.Label();
+            this.plusRightLabel = new System.Windows.Forms.Label();
+            this.Plus = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.NumericUpDown();
+            this.dividedLeftLabel = new System.Windows.Forms.Label();
+            this.Divide = new System.Windows.Forms.Label();
+            this.dividedRightLabel = new System.Windows.Forms.Label();
+            this.Equal2 = new System.Windows.Forms.Label();
+            this.quotient = new System.Windows.Forms.NumericUpDown();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.timeLabel.Location = new System.Drawing.Point(231, 34);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(200, 30);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(116, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "剩余时间";
+            // 
+            // plusLeftLabel
+            // 
+            this.plusLeftLabel.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.plusLeftLabel.Location = new System.Drawing.Point(50, 75);
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            this.plusLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusLeftLabel.TabIndex = 2;
+            this.plusLeftLabel.Text = "?";
+            this.plusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Equal1
+            // 
+            this.Equal1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Equal1.Location = new System.Drawing.Point(248, 75);
+            this.Equal1.Name = "Equal1";
+            this.Equal1.Size = new System.Drawing.Size(60, 50);
+            this.Equal1.TabIndex = 4;
+            this.Equal1.Text = "=";
+            this.Equal1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusRightLabel
+            // 
+            this.plusRightLabel.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.plusRightLabel.Location = new System.Drawing.Point(182, 75);
+            this.plusRightLabel.Name = "plusRightLabel";
+            this.plusRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusRightLabel.TabIndex = 5;
+            this.plusRightLabel.Text = "?";
+            this.plusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Plus
+            // 
+            this.Plus.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Plus.Location = new System.Drawing.Point(116, 75);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(60, 50);
+            this.Plus.TabIndex = 6;
+            this.Plus.Text = "+";
+            this.Plus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sum
+            // 
+            this.sum.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.sum.Location = new System.Drawing.Point(314, 86);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(100, 30);
+            this.sum.TabIndex = 7;
+            this.sum.Enter += new System.EventHandler(this.answer_enter);
+            // 
+            // dividedLeftLabel
+            // 
+            this.dividedLeftLabel.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dividedLeftLabel.Location = new System.Drawing.Point(50, 125);
+            this.dividedLeftLabel.Name = "dividedLeftLabel";
+            this.dividedLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.dividedLeftLabel.TabIndex = 8;
+            this.dividedLeftLabel.Text = "?";
+            this.dividedLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Divide
+            // 
+            this.Divide.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Divide.Location = new System.Drawing.Point(116, 125);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(60, 50);
+            this.Divide.TabIndex = 9;
+            this.Divide.Text = "/";
+            this.Divide.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dividedRightLabel
+            // 
+            this.dividedRightLabel.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dividedRightLabel.Location = new System.Drawing.Point(182, 125);
+            this.dividedRightLabel.Name = "dividedRightLabel";
+            this.dividedRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.dividedRightLabel.TabIndex = 10;
+            this.dividedRightLabel.Text = "?";
+            this.dividedRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Equal2
+            // 
+            this.Equal2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Equal2.Location = new System.Drawing.Point(248, 125);
+            this.Equal2.Name = "Equal2";
+            this.Equal2.Size = new System.Drawing.Size(60, 50);
+            this.Equal2.TabIndex = 11;
+            this.Equal2.Text = "=";
+            this.Equal2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // quotient
+            // 
+            this.quotient.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.quotient.Location = new System.Drawing.Point(314, 140);
+            this.quotient.Name = "quotient";
+            this.quotient.Size = new System.Drawing.Size(100, 30);
+            this.quotient.TabIndex = 12;
+            this.quotient.Enter += new System.EventHandler(this.answer_enter);
+            // 
+            // StartButton
+            // 
+            this.StartButton.AutoSize = true;
+            this.StartButton.Location = new System.Drawing.Point(186, 252);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(99, 27);
+            this.StartButton.TabIndex = 13;
+            this.StartButton.Text = "开始测验";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 362);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.quotient);
+            this.Controls.Add(this.Equal2);
+            this.Controls.Add(this.dividedRightLabel);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.dividedLeftLabel);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.plusRightLabel);
+            this.Controls.Add(this.Equal1);
+            this.Controls.Add(this.plusLeftLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "MathQuiz";
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.Label Equal1;
+        private System.Windows.Forms.Label plusRightLabel;
+        private System.Windows.Forms.Label Plus;
+        private System.Windows.Forms.NumericUpDown sum;
+        private System.Windows.Forms.Label dividedLeftLabel;
+        private System.Windows.Forms.Label Divide;
+        private System.Windows.Forms.Label dividedRightLabel;
+        private System.Windows.Forms.Label Equal2;
+        private System.Windows.Forms.NumericUpDown quotient;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MathQuiz/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+    {
+        int addend1, addend2;
+
+        int dividend, divisor;
+
+        Random randomizer = new Random();
+        int timeLeft;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            StartButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                StartButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft -= 1;
+                timeLabel.Text = timeLeft + " seconds";
+                if (timeLeft <= 5)
+                    timeLabel.BackColor = Color.Red;
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time is up.";
+                MessageBox.Show("你没有完成训练，可惜。", "Fail");
+                sum.Value = addend1 + addend2;
+                StartButton.Enabled = true;
+            }
+        }
+
+        //开始测验的方法
+        public void StartTheQuiz()
+        {
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            sum.Value = 0;
+           
+            divisor = randomizer.Next(2, 11);
+            int tem = randomizer.Next(2, 11);
+            dividend = tem * divisor;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+            
+            timeLeft = 10;
+            timeLabel.Text = "10 seconds";
+            timer1.Start();
+        }
+
+        private void answer_enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerbox = sender as NumericUpDown;
+
+            if (answerbox != null)
+            {
+                int lengthOfAnswer = answerbox.Value.ToString().Length;
+                answerbox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        //检查答案是否正确
+        public bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+                return true;
+            else
+                return false;
+        }
+    }
+}
