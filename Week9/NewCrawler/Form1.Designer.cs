c82c360c1ee02f7aa57288ccd60d8bb0a25d5eb5
嘉程 张 <694510779@qq.com>
2020-05-26 22:54:54 +0800

    第9周代码

---
+++ Week9/NewCrawler/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace NewCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtUrl);
+            this.flowLayoutPanel1.Controls.Add(this.btnStart);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(10, 10, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(664, 64);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(20, 20);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(10);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(324, 21);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "http://www.cnblogs.com/dstang2000/";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(364, 20);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "开始程序";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.index,
+            this.URL,
+            this.status});
+            this.dgvResult.Location = new System.Drawing.Point(12, 101);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.RowTemplate.Height = 23;
+            this.dgvResult.Size = new System.Drawing.Size(641, 326);
+            this.dgvResult.TabIndex = 1;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(12, 76);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 12);
+            this.lblInfo.TabIndex = 2;
+            // 
+            // index
+            // 
+            this.index.DataPropertyName = "index";
+            this.index.HeaderText = "序列号";
+            this.index.Name = "index";
+            // 
+            // URL
+            // 
+            this.URL.DataPropertyName = "URL";
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.Width = 400;
+            // 
+            // status
+            // 
+            this.status.DataPropertyName = "Status";
+            this.status.HeaderText = "状态";
+            this.status.Name = "status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 439);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.dgvResult);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dgvResult;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+    }
+}
+
